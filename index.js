0d49f2d8251da743822c5ce5581ffe600adead4d
Isa Nobu <isanobu1@gmail.com>
2024-06-24 04:00:51 +0600

    get individual items done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +26,13 @@ async function run() {
     app.get("/items", async (req, res) => {
       const cursor = items.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await items.findOne(query);
       res.send(result);
     });
 
