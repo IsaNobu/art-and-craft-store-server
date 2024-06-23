62ba45b1a9327c3ee03ed5da2e843f1c0b70d463
Isa Nobu <isanobu1@gmail.com>
2024-06-24 02:25:08 +0600

    read products

--- index.js
+++ index.js
@@ -22,12 +22,18 @@ async function run() {
   try {
     await client.connect();
 
+    const items = client.db("Doodle_Nation").collection("Popular_Products");
+    app.get("/items", async (req, res) => {
+      const cursor = items.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    await client.close();
   }
 }
 run().catch(console.dir);
