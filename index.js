7c8cdc98f35d3c77f711fba4fe07255fbf149219
Isa Nobu <isanobu1@gmail.com>
2024-06-26 16:08:50 +0600

    view item details page done

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/item-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userData.findOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
